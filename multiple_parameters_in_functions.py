2b59eb4e8ea3288b6760ebafdac8340307d40e2a
DND-N3RD-Al3x <darklord9142@gmail.com>
2024-08-30 12:06:38 -0700

    exported from Skill Struck

---
+++ multiple_parameters_in_functions.py
@@ -0,0 +1,10 @@
+fruit1 = int(input("give me a number: "))
+fruit2 = int(input("give me a second number: "))
+def gifts(first,second):
+    if first > second:
+        print(second)
+    else: 
+        print(first)
+    
+
+gifts(fruit1,fruit2)
