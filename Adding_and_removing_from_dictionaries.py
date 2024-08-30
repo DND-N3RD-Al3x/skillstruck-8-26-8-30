b641183c6f48070732ffbb76f147948b9b1f3d25
DND-N3RD-Al3x <darklord9142@gmail.com>
2024-08-30 12:06:39 -0700

    exported from Skill Struck

---
+++ Adding_and_removing_from_dictionaries.py
@@ -0,0 +1,8 @@
+trees = { "aspen" : 75, "pine" : 82, "maple" : 60, "oak" : 65, "willow" : 80, "cottonwood" : 100 }
+
+remove = input("what type of trees do you want to remove: ")
+
+trees.pop(remove)
+
+print(trees)
+
