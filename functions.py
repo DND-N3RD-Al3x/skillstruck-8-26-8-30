67597b649b853e0d66c6a4ee1126765b34c058d6
DND-N3RD-Al3x <darklord9142@gmail.com>
2024-08-30 12:06:37 -0700

    exported from Skill Struck

---
+++ functions.py
@@ -0,0 +1,7 @@
+start = input("Do you want to start the game? ")
+def ask():
+    if start == "yes":
+        print("Initialization Complete")
+    else:
+        print("Initialization Failed")
+ask()
